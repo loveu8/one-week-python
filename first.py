05bc9958c962fff8571c5a5e38cf17d02326a142
SL-ASUSROGGM501\StarQk <qkpigstar@gmail.com>
2025-03-16 19:54:31 +1000

    first commit

---
+++ first.py
@@ -0,0 +1,33 @@
+# 2025/03/16 Lorn
+# print test
+print(1+2+3+4)
+print("Hello World!!!")
+
+# Basic Data Type Integers Floats Strings Booleans
+# Integer
+1
+2
+1561561515165165156156165
+-99
+# check type
+type(1)
+
+# float
+1.1
+-0.3
+0.415415615156156
+# check type
+type(2.5)
+
+# float careful!!
+# result 3.3333333333333335
+3.333333333333333333333333333333333333333333
+
+# it is ok
+# 3100000000
+31_000_000_000
+
+# 1000.5
+1_000.5
+
+# <- This is comment
