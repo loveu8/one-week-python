2342aaa97a783118b1d75a5d5d088406711250f1
SL-ASUSROGGM501\StarQk <qkpigstar@gmail.com>
2025-03-25 21:34:09 +1000

    學習integer

--- variable.py
+++ variable.py
@@ -1,1 +1,14 @@
 # learn variable
+# left age is variable 
+# = assign the value for left varible
+# right num is some number we like to give
+# Integer value
+age = 104
+print(age)
+print(age + 1)
+print(age)
+
+score = 5000
+print(score)
+score = score * 3
+print(score)
