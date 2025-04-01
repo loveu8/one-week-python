f51cc1136dd21954d12e1e910acb7a889953ba70
SL-ASUSROGGM501\StarQk <qkpigstar@gmail.com>
2025-04-01 20:30:11 +1000

    learn operators and exercise

---
+++ operators.py
@@ -0,0 +1,21 @@
+# Assignment Operators
+# +=
+prize_money = 500
+prize_money = prize_money + 50
+print(prize_money)
+prize_money += 75
+print(prize_money)
+# -=
+prize_money -= 55
+print(prize_money)
+# *=
+prize_money *= 10
+print(prize_money)
+# /= , it will change to float
+prize_money /= 10
+print(prize_money)
+print(type(prize_money))
+# carefully
+print(prize_money - 1)
+# this will not change
+print(prize_money)
