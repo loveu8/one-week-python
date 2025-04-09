34c19f3db3fcf93cbf53151717851b53035650eb
SL-ASUSROGGM501\StarQk <qkpigstar@gmail.com>
2025-04-09 21:42:26 +1000

    learn strings

---
+++ strings.py
@@ -0,0 +1,31 @@
+# example
+helleword = "hello word"
+print(helleword)
+print(type(helleword))
+
+# error
+# 'I can't love you anymore'
+# ok
+# "I can't love you anymore"
+# ok
+# 'he said "haha"'
+
+# python can change data type
+age = 85
+print(type(age))
+age = "85"
+print(type(age))
+
+# String Operators
+# + -> plus
+# String uses plus operator is concat the string
+print("hello" + " " + "world")
+last_name = "jhu"
+first_name = "lorn"
+print(last_name + " " + first_name)
+
+# * -> multuply
+print("ha" * 3)
+
+# error , because data type diff
+# "ha" + 3
