98863d7ae2581f87579b19b47ca49828a73cad77
SL-ASUSROGGM501\StarQk <qkpigstar@gmail.com>
2025-04-14 20:19:27 +1000

    string basic

--- strings.py
+++ strings.py
@@ -29,3 +29,26 @@ print("ha" * 3)
 
 # error , because data type diff
 # "ha" + 3
+
+# String Are Ordered/Indexed
+# first one start with 0
+# 0 1 2 3 4
+# H e L L O
+msg = "I <3 Cats"
+print(msg[0])
+print(msg[5])
+print("hello"[1])
+# Erro
+# string index out of range
+# msg[99]
+
+# other o rdered
+#  0  1  2  3  4  5  6  7  8
+# -9 -8 -7 -6 -5 -4 -3 -2 -1
+#  I     <  3     C  a  t  s
+print(msg[-1])
+
+# None
+# It is not the same as Zero or ""
+none = None
+print(type(none))
