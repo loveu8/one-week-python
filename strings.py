0cc8292e4b23851d0dc2bce477b48d1a005e4f9f
SL-ASUSROGGM501\StarQk <qkpigstar@gmail.com>
2025-08-28 21:59:01 +1000

    練習

--- strings.py
+++ strings.py
@@ -52,3 +52,41 @@ print(msg[-1])
 # It is not the same as Zero or ""
 none = None
 print(type(none))
+
+# string
+address ='''
+hello
+world !!!!!!!!!
+
+"I can't believe you"
+'''
+
+print(address)
+
+
+# Exercise
+
+first = "Nico"
+last = "Hulkenberg"
+
+# - Create a variable called "full_name" that combines first and last with a space between them.  
+# - Print it out
+full_name = first + " " + last
+print(full_name)
+
+
+# - Create an "initials" variable that holds the first character of first followed by the first character of last. 
+# - Print it out
+initials = first[0] + last[0]
+print(initials)
+
+
+# - Create an "initials_2" variable that holds the first character of first followed by the first character of last, with periods after each letter!
+# - Print it out
+initials_2 = first[0] + "." + last[0] + "."
+print(initials_2)
+
+# Create a "nickname" variable that holds the first 4 characters of "last" (use a slice)
+# Print it out 
+nickname = last[:4]
+print(nickname)
