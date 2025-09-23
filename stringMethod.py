98abf2901222caeb98603b9e88e84f4989f1e47a
SL-ASUSROGGM501\StarQk <qkpigstar@gmail.com>
2025-09-23 20:59:59 +1000

    add comment

--- stringMethod.py
+++ stringMethod.py
@@ -1,5 +1,5 @@
 # example
-# doc : https://docs.python.org/3/library/stdtypes.html#string-methods
+# doc : https://docs.python.org/3/library/stdtypes.html#string-methods            ㄖ施      
 # thing.method()
 print("test".upper());
 print("QQQQ".lower());
